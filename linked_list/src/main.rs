23270bfd02965432475210391ba2babfe2cc853e
Ruslan Radetskiy <russ2510@gmail.com>
2025-07-08 20:56:23 -0400

    intial commit

---
+++ linked_list/src/main.rs
@@ -0,0 +1,19 @@
+struct SinglyLinkListNode<T>{
+    val: T,
+    node: Option<Box<SinglyLinkListNode<T>>>
+}
+
+struct SinglyLinkList<T>{
+    length: u16,
+    head: SinglyLinkListNode<T>
+}
+
+impl SinglyLinkList<T>{
+    fn createSinglyLinkList(val: T) -> SinglyLinkList<T>{
+        newHead = 
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
