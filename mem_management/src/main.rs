803e129ea42bd3d5eb3ee51afec9939d50e92c5e
Ruslan Radetskiy <russ2510@gmail.com>
2025-07-11 19:20:59 -0400

    Starting to understand pointers in rust

--- mem_management/src/main.rs
+++ mem_management/src/main.rs
@@ -1,5 +1,31 @@
+use std::{cell::RefCell, clone, rc::Rc};
 
+#[derive(Debug, Clone)]
+struct Person{
+    age:u8,
+    name: String
+}
+
+impl Person {
+    fn set_age(&mut self, new_age: u8){
+        self.age = new_age;
+    }
+
+     fn set_name(&mut self, new_name: String){
+        self.name = new_name;
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+    let person = Rc::new(RefCell::new(Person {age: 10, name: String::from("test")}));
+
+    //person.set_age(8);
+    
+    let p = Rc::clone(&person);
+    //p.name = String::from("changed");
+    let p2 = Rc::clone(&p);
+    person.borrow_mut().set_age(8);
+    println!("the address of p is {:p} \n {:?}", &p, p);
+    println!("the address of p2 is {:p} \n {:?}", &p2, p2);
+    println!("the address of person is {:p} \n {:?}", &person, person);
 }
